2c1d2764946d8bd32610ce0b8f3b1cca1f6b2de9
Ivelin Bratanov <ivelin.bratanov@ericsson.com>
2014-04-02 19:24:17 -0400

    Cleaned up code and set indenting to 2 spaces.
    Removed comments and included a backup of popup.js with old
    code. Changed function names and added a line to change button
    text back to "Slap!" after "Unslap!"
    tldr: not much.

--- popup.js
+++ popup.js
@@ -11,123 +11,7 @@ Leap.loop(controllerOptions, function(frame) {
     return; // Skip this update
   }
 
-  // // Display Frame object data
-  // var frameOutput = document.getElementById("frameData");
-
-   var frameString = 
-   //"Frame ID: " + frame.id  + "<br />"
-  //                 + "Timestamp: " + frame.timestamp + " &micro;s<br />"
-  //                 + "Hands: " + frame.hands.length + "<br />"
-  //                 + "Fingers: " + frame.fingers.length + "<br />"
-  //                 + "Tools: " + frame.tools.length + "<br />"
-                  "Gestures: " + frame.gestures.length + "<br />";
-
-  // // Frame motion factors
-  // if (previousFrame && previousFrame.valid) {
-  //   var translation = frame.translation(previousFrame);
-  //   frameString += "Translation: " + vectorToString(translation) + " mm <br />";
-
-  //   var rotationAxis = frame.rotationAxis(previousFrame);
-  //   var rotationAngle = frame.rotationAngle(previousFrame);
-  //   frameString += "Rotation axis: " + vectorToString(rotationAxis, 2) + "<br />";
-  //   frameString += "Rotation angle: " + rotationAngle.toFixed(2) + " radians<br />";
-
-  //   var scaleFactor = frame.scaleFactor(previousFrame);
-  //   frameString += "Scale factor: " + scaleFactor.toFixed(2) + "<br />";
-  // }
-  // frameOutput.innerHTML = "<div style='width:300px; float:left; padding:5px'>" + frameString + "</div>";
-
-  // // Display Hand object data
-  // var handOutput = document.getElementById("handData");
-  // var handString = "";
-  // if (frame.hands.length > 0) {
-  //   for (var i = 0; i < frame.hands.length; i++) {
-  //     var hand = frame.hands[i];
-
-  //     handString += "<div style='width:300px; float:left; padding:5px'>";
-  //     handString += "Hand ID: " + hand.id + "<br />";
-  //     handString += "Direction: " + vectorToString(hand.direction, 2) + "<br />";
-  //     handString += "Palm normal: " + vectorToString(hand.palmNormal, 2) + "<br />";
-  //     handString += "Palm position: " + vectorToString(hand.palmPosition) + " mm<br />";
-  //     handString += "Palm velocity: " + vectorToString(hand.palmVelocity) + " mm/s<br />";
-  //     handString += "Sphere center: " + vectorToString(hand.sphereCenter) + " mm<br />";
-  //     handString += "Sphere radius: " + hand.sphereRadius.toFixed(1) + " mm<br />";
-
-  //     // Hand motion factors
-  //     if (previousFrame && previousFrame.valid) {
-  //       var translation = hand.translation(previousFrame);
-  //       handString += "Translation: " + vectorToString(translation) + " mm<br />";
-
-  //       var rotationAxis = hand.rotationAxis(previousFrame, 2);
-  //       var rotationAngle = hand.rotationAngle(previousFrame);
-  //       handString += "Rotation axis: " + vectorToString(rotationAxis) + "<br />";
-  //       handString += "Rotation angle: " + rotationAngle.toFixed(2) + " radians<br />";
-
-  //       var scaleFactor = hand.scaleFactor(previousFrame);
-  //       handString += "Scale factor: " + scaleFactor.toFixed(2) + "<br />";
-  //     }
-
-  //     // IDs of pointables (fingers and tools) associated with this hand
-  //     if (hand.pointables.length > 0) {
-  //       var fingerIds = [];
-  //       var toolIds = [];
-  //       for (var j = 0; j < hand.pointables.length; j++) {
-  //         var pointable = hand.pointables[j];
-  //         if (pointable.tool) {
-  //           toolIds.push(pointable.id);
-  //         }
-  //         else {
-  //           fingerIds.push(pointable.id);
-  //         }
-  //       }
-  //       if (fingerIds.length > 0) {
-  //         handString += "Fingers IDs: " + fingerIds.join(", ") + "<br />";
-  //       }
-  //       if (toolIds.length > 0) {
-  //         handString += "Tools IDs: " + toolIds.join(", ") + "<br />";
-  //       }
-  //     }
-
-  //     handString += "</div>";
-  //   }
-  // }
-  // else {
-  //   handString += "No hands";
-  // }
-  // handOutput.innerHTML = handString;
-
-  // Display Pointable (finger and tool) object data
-  // var pointableOutput = document.getElementById("pointableData");
-  // var pointableString = "";
-  // if (frame.pointables.length > 0) {
-  //   for (var i = 0; i < frame.pointables.length; i++) {
-  //     var pointable = frame.pointables[i];
-
-  //     pointableString += "<div style='width:250px; float:left; padding:5px'>";
-  //     pointableString += "Pointable ID: " + pointable.id + "<br />";
-  //     pointableString += "Belongs to hand with ID: " + pointable.handId + "<br />";
-
-  //     if (pointable.tool) {
-  //       pointableString += "Classified as a tool <br />";
-  //       pointableString += "Length: " + pointable.length.toFixed(1) + " mm<br />";
-  //       pointableString += "Width: "  + pointable.width.toFixed(1) + " mm<br />";
-  //     }
-  //     else {
-  //       pointableString += "Classified as a finger<br />";
-  //       pointableString += "Length: " + pointable.length.toFixed(1) + " mm<br />";
-  //     }
-
-  //     pointableString += "Direction: " + vectorToString(pointable.direction, 2) + "<br />";
-  //     pointableString += "Tip position: " + vectorToString(pointable.tipPosition) + " mm<br />";
-  //     pointableString += "Tip velocity: " + vectorToString(pointable.tipVelocity) + " mm/s<br />";
-
-  //     pointableString += "</div>";
-  //   }
-  // }
-  // else {
-  //   pointableString += "<div>No pointables</div>";
-  // }
-  // pointableOutput.innerHTML = pointableString;
+  var frameStringi = "Gestures: " + frame.gestures.length + "<br />";
 
   // Display Gesture object data
   var gestureOutput = document.getElementById("gestureData");
@@ -145,37 +29,15 @@ Leap.loop(controllerOptions, function(frame) {
                     + "hand IDs: " + gesture.handIds.join(", ") + ", "
                     + "pointable IDs: " + gesture.pointableIds.join(", ") + ", "
                     + "duration: " + gesture.duration + " &micro;s, ";
-          if (gesture.type == "swipe"){
-		      slapAnimation();
-          }
-      // switch (gesture.type) {
-      //   case "circle":
-      //     gestureString += "center: " + vectorToString(gesture.center) + " mm, "
-      //                   + "normal: " + vectorToString(gesture.normal, 2) + ", "
-      //                   + "radius: " + gesture.radius.toFixed(1) + " mm, "
-      //                   + "progress: " + gesture.progress.toFixed(2) + " rotations";
-      //     break;
-      //   case "swipe":
-      //     gestureString += "start position: " + vectorToString(gesture.startPosition) + " mm, "
-      //                   + "current position: " + vectorToString(gesture.position) + " mm, "
-      //                   + "direction: " + vectorToString(gesture.direction, 2) + ", "
-      //                   + "speed: " + gesture.speed.toFixed(1) + " mm/s";
-      //     break;
-      //   case "screenTap":
-      //   case "keyTap":
-      //     gestureString += "position: " + vectorToString(gesture.position) + " mm, "
-      //                   + "direction: " + vectorToString(gesture.direction, 2);
-      //     break;
-      //   default:
-      //     gestureString += "unkown gesture type";
-      // }
+      if (gesture.type == "swipe"){
+        slapAnimation();
+      }
       gestureString += "<br />";
     }
   }
   else {
     gestureString += "No gestures";
   }
-  // gestureOutput.innerHTML = gestureString;
 
   // Store frame for motion functions
   previousFrame = frame;
@@ -184,61 +46,56 @@ Leap.loop(controllerOptions, function(frame) {
 var slapped = false;
 
 function slapAnimation() {
-            if (slapped == false){
+  if (slapped == false) {
 
-            console.log("slapped");
+  console.log("slapped");
  
-            togglePause();
+  togglePause();
 
-            document.getElementById("slap").src="./images/slap.gif";
+  document.getElementById("slap").src="./images/slap.gif";
 
+  var sound = new Audio('./Voice0004.mp3');
+  sound.play();
+  document.getElementById("sorry").style.visibility="visible";
 
-            var sound = new Audio('./Voice0004.mp3');
-            sound.play();
-	         document.getElementById("sorry").style.visibility="visible";
+  document.getElementById("slapbutton").innerText = "Unslap!";
+  setTimeout(function() { standbyReversed();}, 2400);
 
-            document.getElementById("slapbutton").innerText = "Unslap!";
-            setTimeout(function() { temp();}, 2400);
-
-            slapped = true;
-          }
-
-          else {
-              console.log("reverse slapped");
+  slapped = true;
+  } else {
+    console.log("reverse slapped");
  
-            togglePause();
+    togglePause();
 
-            document.getElementById("slap").src="./images/slap-reversed.gif";
+    document.getElementById("slap").src="./images/slap-reversed.gif";
 
+    var sound = new Audio('./Voice0004.mp3');
+    sound.play();
+    document.getElementById("sorry").style.visibility="visible";
 
-            var sound = new Audio('./Voice0004.mp3');
-            sound.play();
-           document.getElementById("sorry").style.visibility="visible";
+    document.getElementById("slapbutton").innerText = "Slap!";
+    setTimeout(function() { standby();}, 2500);
 
-            setTimeout(function() { temp2();}, 2500);
+    slapped = false;
 
-            slapped = false;
-
-          }
-
-            
+  }           
 }
 
-function temp() {
-    document.getElementById("slap").src="./images/slap-reversed-still.png";
-    document.getElementById("sorry").style.visibility="hidden";
+function standbyReversed() {
+  document.getElementById("slap").src="./images/slap-reversed-still.png";
+  document.getElementById("sorry").style.visibility="hidden";
 }
 
-function temp2() {
-    document.getElementById("slap").src="./images/slap-still.png";
-    document.getElementById("sorry").style.visibility="hidden";
+function standby() {
+  document.getElementById("slap").src="./images/slap-still.png";
+  document.getElementById("sorry").style.visibility="hidden";
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-    var button = document.getElementById("slapbutton");
-    button.addEventListener('click', function() {
-        slapAnimation();
-    });
+  var button = document.getElementById("slapbutton");
+  button.addEventListener('click', function() {
+    slapAnimation();
+  });
 });
 
 function vectorToString(vector, digits) {
